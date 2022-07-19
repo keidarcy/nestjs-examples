f0e5109c67df8dc1e3803253f8a0ada3ca706caa
Xing Yahao <keidarcy.1015@gmail.com>
2022-07-19 18:42:59 +0900

    test 2

--- prisma-mongodb/src/app.service.ts
+++ prisma-mongodb/src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AppService {
