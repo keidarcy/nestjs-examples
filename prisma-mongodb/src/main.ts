aafe4a4e172fa5a2b6c4a801ad80cd99928bab6a
Xing Yahao <keidarcy.1015@gmail.com>
2022-07-20 18:31:55 +0900

    Add prisma mongodb to nestjs

--- prisma-mongodb/src/main.ts
+++ prisma-mongodb/src/main.ts
@@ -6,7 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
-
   await app.listen(3000);
 }
 bootstrap();
